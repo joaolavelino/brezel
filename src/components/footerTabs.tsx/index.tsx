f43a294b1638d7c0e6f0b2767c73140b44dba3e6
João Avelino <joaolavelino@gmail.com>
2026-03-26 15:25:04 -0300

    feat: tabs animated with framer

--- src/components/footerTabs.tsx/index.tsx
+++ src/components/footerTabs.tsx/index.tsx
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import { SearchContent } from "./SearchContent";
 import { CaptureContent } from "./CaptureContent";
+import { AnimatePresence, motion, Transition } from "framer-motion";
 
 export function FooterTabs() {
   const [mode, setMode] = useState<null | "capture" | "search">(null);
@@ -19,40 +20,62 @@ export function FooterTabs() {
     setMode(null);
   };
 
+  //Animation
+  const springConfig: Transition = {
+    type: "spring",
+    stiffness: 300,
+    damping: 30,
+  };
+
   return (
     <div className="fixed w-full bottom-0">
-      <div
-        className={`bg-surface-muted pt-2  rounded-t-4xl px-8 relative top-4 z-10 overflow-hidden flex flex-col ${
-          mode === "search"
-            ? "max-h-[calc(100dvh-80px)] h-[calc(100dvh-80px)] pb-8"
-            : mode === "capture"
-            ? "h-0 pb-6"
-            : "h-0 pb-20"
-        }`}
-        style={{
-          transition: "height 300ms cubic-bezier(0.4, 0, 0.2, 1)",
-        }}
-      >
-        <SearchContent
-          isCaptureMode={mode === "capture"}
-          isSearchMode={mode === "search"}
-          disableSearchMode={disableSearch}
-          enableSearchMode={enableSearch}
-        />
-      </div>
-      <div
-        className={`text-center bg-primary text-text-fixed-light py-2 rounded-t-4xl overflow-hidden relative z-20 max-h-[calc(100dvh-40px)] ${
-          mode === "capture" ? "h-[calc(100dvh-40px)]" : "h-16"
-        }`}
-        style={{
-          transition: "height 300ms cubic-bezier(0.4, 0, 0.2, 1)",
-        }}
-      >
-        <CaptureContent
-          isCaptureMode={mode == "capture"}
-          toggleCapture={toggleCapture}
-        />
-      </div>
+      <AnimatePresence>
+        <motion.div
+          key="searchTab"
+          transition={springConfig}
+          initial={false}
+          animate={{
+            // Se for search: sobe tudo.
+            // Se for capture: desce 100% (some).
+            // Se for null: fica com 64px (h-16) no rodapé.
+            height:
+              mode === "search"
+                ? "calc(100dvh - 80px)"
+                : mode === "capture"
+                ? 0
+                : "64px",
+            y: mode === "capture" ? "100%" : 0,
+          }}
+          className={`bg-surface-muted pt-2  rounded-t-4xl px-8 top-6 relative z-10 overflow-hidden flex flex-col ${
+            mode === "search" ? "pb-8" : mode === "capture" ? "pb-20" : "pb-20 "
+          }`}
+        >
+          <SearchContent
+            isCaptureMode={mode === "capture"}
+            isSearchMode={mode === "search"}
+            disableSearchMode={disableSearch}
+            enableSearchMode={enableSearch}
+          />
+        </motion.div>
+        <motion.div
+          key="captureTab"
+          transition={springConfig}
+          initial={false}
+          animate={{
+            // Se for capture: sobe. Se for search: desce 100%. Se null: 64px.
+            height: mode === "capture" ? "calc(100dvh - 40px)" : "64px",
+            // y: mode === "search" ? "100%" : 0,
+          }}
+          className={`text-center bg-primary text-text-fixed-light py-2 rounded-t-4xl overflow-hidden relative z-20 ${
+            mode === "capture" ? "" : "h-16"
+          }`}
+        >
+          <CaptureContent
+            isCaptureMode={mode == "capture"}
+            toggleCapture={toggleCapture}
+          />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
